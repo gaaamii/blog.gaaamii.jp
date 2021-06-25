593a041d3649c3e10ded7df749f0b5f90e865779
gaaamii <i1shi4ga4mi1@gmail.com>
2021-06-25 23:22:27 +0900

    Tweak styles

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,6 +1,6 @@
 import styles from './styles.module.css'
 
-const Main = ({ children }: { children: React.ReactElement }) => (
+const Main = ({ children }: { children: React.ReactNode }) => (
   <main className={styles.root}>
     {children}
   </main>
