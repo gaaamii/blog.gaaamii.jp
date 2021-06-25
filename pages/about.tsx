8565a7e95845c530d6007aaef82498124e82a9e3
gaaamii <i1shi4ga4mi1@gmail.com>
2021-06-25 23:22:49 +0900

    Add /about page

---
+++ pages/about.tsx
@@ -0,0 +1,58 @@
+import { GetStaticPropsContext } from 'next'
+import Head from 'next/head'
+import Main from '../components/Main/index';
+import PageLink from '../components/PageLink/index';
+import { Post } from '../models/post';
+import { get } from '../utils/api';
+import { NavigationHeader } from '../components/NavigationHeader/index';
+
+type Props = {
+  posts?: Post[];
+}
+
+export default function Home(props: Props) {
+  return (
+    <>
+      <Head>
+        <title>gaaamiiのブログ</title>
+        <meta name="description" content="間違ったことを書いている時があります。コメントやTwitter、ブコメなどでご指摘ください" />
+      </Head>
+
+      <NavigationHeader />
+      <Main>
+        <h1>このブログについて</h1>
+        <p>
+          @gaaamiiが書いているブログです。個人的な日記やウェブ開発周りの雑記を、気が向いたときに書いています。
+        </p>
+      </Main>
+    </>
+  )
+}
+
+const Posts = (props: Props) => {
+  if (!props.posts) {
+    return (
+      <div>読み込み中...</div>
+    )
+  }
+
+  return (
+    <ul>
+      {props.posts.map(post => (
+        <PageLink href={`/posts/${post.id}`} published_at={post.published_at} title={post.title} key={post.id} />
+      ))}
+    </ul>
+  )
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const res = await get(`/posts`)
+  const posts = res.ok ? await res.json() : []
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 1,
+  }
+}
