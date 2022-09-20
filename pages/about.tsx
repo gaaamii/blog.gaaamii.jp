0a6eeddc9d3cdf8daf324ca9f2d80ac88f2fea63
gaaamii <i1shi4ga4mi1@gmail.com>
2022-09-20 22:11:53 +0900

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,6 +5,7 @@ import PageLink from '../components/PageLink/index';
 import { Post } from '../models/post';
 import { get } from '../utils/api';
 import { NavigationHeader } from '../components/NavigationHeader/index';
+import Link from 'next/link';
 
 type Props = {
   posts?: Post[];
@@ -20,9 +21,15 @@ export default function Home(props: Props) {
 
       <NavigationHeader />
       <Main>
-        <h1>このブログについて</h1>
+        <h1 className='mt-16 font-bold'>このブログについて</h1>
         <p>
-          @gaaamiiが書いているブログです。個人的な日記やウェブ開発周りの雑記を、気が向いたときに書いています。
+          東京都在住のソフトウェアエンジニアが書いているブログです。個人的な日記やソフトウェア開発周りの雑記を、気が向いたときに書いています。
+          <br />
+        </p>
+        <p className="text-right">
+          <Link href="https://gaaamii.jp">
+            <a className="underline font-bold text-sky-700">著者詳細はこちら</a>
+          </Link>
         </p>
       </Main>
     </>
