59a7ed07ddb13356820eae2bab24c3ea40dfa7a6
gaaamii <i1shi4ga4mi1@gmail.com>
2022-11-12 14:55:22 +0900

    update

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -44,9 +44,9 @@ const h2Component = ({ children }) => (
 const h3Component = ({ children }) => (
    <h3 className={"font-bold text-lg mt-8 pl-6 border-l-4 border-black"}>{children}</h3>
 )
-const pComponent = ({ children }) => (
-   <p className={"mt-6 leading-8"}>{children}</p>
-)
-const aComponent = ({ node, children, href }) => (
-   <a className={"font-bold underline hover:text-sky-600"} href={href} target="blank_">{children}</a>
+const pComponent = ({ children, node, className, ...props }) => {
+   return <p className={"mt-6 leading-8"} {...props}>{children}</p>
+}
+const aComponent = ({ node, children, href, ...props }) => (
+   <a className={"font-bold underline hover:text-sky-600"} href={href} target="blank_" {...props}>{children}</a>
 )
