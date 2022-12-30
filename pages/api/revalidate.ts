9950a530b8a7580514f6829a012bf4cc1c309aaa
gaaamii <i1shi4ga4mi1@gmail.com>
2022-12-30 22:14:02 +0900

    Revalidate also index page from revalidate endpoint

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 
   try {
     await res.revalidate(`/posts/${req.query.post_id}`)
+    await res.revalidate(`/posts`)
     return res.json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
