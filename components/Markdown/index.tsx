2fa70579c5546aa1dcf0a3c380503e0e5aae0ac3
gaaamii <i1shi4ga4mi1@gmail.com>
2023-09-19 15:08:17 +0900

    remove rehype-raw

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,51 +1,52 @@
-import Script from 'next/script';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import styles from './styles.module.css'
-import { useEffect, useState } from 'react';
-import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import rehypeRaw from 'rehype-raw';
+import Script from "next/script";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import styles from "./styles.module.css";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 type Props = {
   mdxSource: MDXRemoteSerializeResult;
-}
+};
 
 export const MarkdownCompiledOnServer = ({ mdxSource }: Props) => {
-  return <MDXRemote
-    {...mdxSource}
-    components={{
-      code: codeComponent,
-      script: scriptComponent,
-      h2: h2Component,
-      h3: h3Component,
-      p: pComponent,
-      a: aComponent
-    }}
-  />
-}
+  return (
+    <MDXRemote
+      {...mdxSource}
+      components={{
+        code: codeComponent,
+        h2: h2Component,
+        h3: h3Component,
+        p: pComponent,
+        a: aComponent,
+      }}
+    />
+  );
+};
 
-export const MarkdownCompiledOnClient = ({ children }: { children: string }) => {
-  return <ReactMarkdown
-    children={children}
-    components={{
-      code: codeComponent,
-      script: scriptComponent,
-      h2: h2Component,
-      h3: h3Component,
-      p: pComponent,
-      a: aComponent
-    }}
-    rehypePlugins={[rehypeRaw]}
-  />
-}
+export const MarkdownCompiledOnClient = ({
+  children,
+}: {
+  children: string;
+}) => {
+  return (
+    <ReactMarkdown
+      children={children}
+      components={{
+        code: codeComponent,
+        h2: h2Component,
+        h3: h3Component,
+        p: pComponent,
+        a: aComponent,
+      }}
+    />
+  );
+};
 
-const scriptComponent = props => <Script {...props} />
-
-import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 const codeComponent = ({ node, inline, className, children, ...props }) => {
   // NOTE: CodeComponent's className is typed as unknown
-  const classNameString = className as string
-  const match = /language-(\w+)/.exec(classNameString || '')
+  const classNameString = className as string;
+  const match = /language-(\w+)/.exec(classNameString || "");
   return !inline && match ? (
     <SyntaxHighlighter
       style={a11yDark}
@@ -53,21 +54,32 @@ const codeComponent = ({ node, inline, className, children, ...props }) => {
       PreTag="div"
       {...props}
     >
-      {String(children).replace(/\n$/, '')}
+      {String(children).replace(/\n$/, "")}
     </SyntaxHighlighter>
   ) : (
     <code className={styles.plainCode}>{children}</code>
-  )
-}
+  );
+};
 const h2Component = ({ children }) => (
-   <h2 className={"font-bold text-xl mt-10"}>{children}</h2>
-)
+  <h2 className={"font-bold text-xl mt-10"}>{children}</h2>
+);
 const h3Component = ({ children }) => (
-   <h3 className={"font-bold text-lg mt-8 pl-6 border-l-4 border-black"}>{children}</h3>
-)
+  <h3 className={"font-bold text-lg mt-8 pl-6 border-l-4 border-black"}>
+    {children}
+  </h3>
+);
 const pComponent = ({ children, node, className, ...props }) => (
- <p className={className || "mt-6 leading-8"} {...props}>{children}</p>
-)
+  <p className={className || "mt-6 leading-8"} {...props}>
+    {children}
+  </p>
+);
 const aComponent = ({ node, children, href, ...props }) => (
-   <a className={"font-bold underline hover:text-sky-600"} href={href} target="blank_" {...props}>{children}</a>
-)
+  <a
+    className={"font-bold underline hover:text-sky-600"}
+    href={href}
+    target="blank_"
+    {...props}
+  >
+    {children}
+  </a>
+);
