62cb9739189c6919ef25d9cdbdd8699f1c3a7e74
gaaamii <i1shi4ga4mi1@gmail.com>
2024-01-27 22:18:13 +0900

    Add Search form

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 
 const Main = ({ children }: { children: React.ReactNode }) => (
-  <main className={`${styles.root} mx-auto sm:my-12`}>{children}</main>
+  <main className={`${styles.root} mx-auto sm:my-8`}>{children}</main>
 );
 
 export default Main;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,85 @@ import PostLink from "../components/PostLink/index";
 import { Post } from "../models/post";
 import { get } from "../utils/api";
 import MainLayout from "../components/layouts/MainLayout";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
   posts?: Post[];
 };
 
 export default function Home(props: Props) {
+  const { query, onChangeQuery } = useHome();
+
   return (
     <MainLayout>
-      <Posts {...props} />
+      <Search onChange={onChangeQuery} />
+      <Posts posts={props.posts} query={query} />
     </MainLayout>
   );
 }
 
-const Posts = (props: Props) => {
-  if (!props.posts) {
+const useHome = () => {
+  const searchParamsQuery = useSearchParams().get("query");
+  const [query, setQuery] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    setQuery(searchParamsQuery);
+  }, [searchParamsQuery]);
+
+  const onChangeQuery = (text: string) => {
+    setQuery(text);
+  };
+
+  return {
+    query,
+    onChangeQuery,
+  };
+};
+
+const Search = ({ onChange }: { onChange: (text: string) => void }) => {
+  return (
+    <form
+      role="search"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      className="flex justify-end w-full mt-4 px-2 sm:mt-0 sm:px-0"
+    >
+      <input
+        type="search"
+        placeholder="記事を検索"
+        aria-label="記事を検索"
+        className="bg-white px-4 py-2 rounded w-full border-2 "
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          e.preventDefault();
+          onChange(e.target.value);
+        }}
+      />
+    </form>
+  );
+};
+
+const matchQuery = (post: Post, query: string) =>
+  post.title.toLowerCase().indexOf(query.toLocaleLowerCase()) >= 0;
+
+type PostsProps = {
+  posts: Props["posts"];
+  query?: string;
+};
+const Posts = ({ posts, query }: PostsProps) => {
+  if (!posts) {
     return <div>読み込み中...</div>;
   }
 
   return (
-    <section className="py-4 sm:p-0">
+    <section className="py-4 xs:p-0 xs:mt-4">
       <ul>
-        {props.posts.map((post) => (
-          <PostLink {...post} key={post.id} />
-        ))}
+        {posts
+          .filter((post) => (query ? matchQuery(post, query) : post))
+          .map((post) => (
+            <PostLink {...post} key={post.id} />
+          ))}
       </ul>
     </section>
   );
