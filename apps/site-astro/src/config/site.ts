868a547529d8f1ae99daf6eeb20b88aa82eeb68b
gaaamii <i1shi4ga4mi1@gmail.com>
2026-09-07 06:40:24 +0900

    feat: migrate public site pages to Astro

---
+++ apps/site-astro/src/config/site.ts
@@ -0,0 +1,8 @@
+export const SITE_TITLE = "gaaamiiのブログ";
+
+export const SITE_DESCRIPTION =
+  "間違ったことを書いている時があります。コメントやTwitter、ブコメなどでご指摘ください";
+
+export const SITE_AUTHOR = "gaaamii";
+
+export const TWITTER_ACCOUNT = "@gaaamii";
